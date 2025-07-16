eb53ce70847bc021b267765875836939b5d3a377
webcatzz <71499717+webcatzz@users.noreply.github.com>
2025-07-16 22:14:48 +0400

    cleanup

--- scenes/route/phases/heat/bullet.gd
+++
@@ -1,8 +0,0 @@
-extends PathFollower
-
-@onready var collision: CollisionShape2D = $Hitbox/Collision
-
-
-func set_disabled(value: bool) -> void:
-	visible = not value
-	collision.disabled = value
--- scenes/route/phases/phase.gd
+++ scenes/route/phases/phase.gd
@@ -4,9 +4,5 @@ extends Node
 @onready var route: Route = get_tree().current_scene
 
 
-func _ready() -> void:
-	await route.wipe_out()
-
-
 func end() -> void:
 	route.wipe_to(preload("res://scenes/route/phases/night/night_phase.tscn"), self)
