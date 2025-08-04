fdd3122d31ed6d4ae854e4e2bade5a7f50df323e
webcatzz <71499717+webcatzz@users.noreply.github.com>
2025-08-04 17:47:04 +0400

    audio normalization + removed night theme from night phase

--- scenes/route/phases/night/night_phase.gd
+++ scenes/route/phases/night/night_phase.gd
@@ -5,15 +5,8 @@ extends Phase
 
 func _ready() -> void:
 	$Background/MoonShadow/Moon/Phase.position.x = randf_range(-20.0, 0.0)
-	$Music.seek(randf_range(0.0, $Music.stream.get_length()))
-	var tween := create_tween().set_parallel()
-	tween.tween_property($Music, ^"volume_linear", 1.0, 0.5).from(0.0)
-	tween.tween_property(route.music, ^"volume_linear", 0.1, 0.5)
 
 
 func end() -> void:
 	map.set_physics_process(false)
-	var tween := create_tween().set_parallel()
-	tween.tween_property($Music, ^"volume_linear", 0.0, 1.0)
-	tween.tween_property(route.music, ^"volume_linear", 1.0, 1.0)
 	route.step(map.branch)
