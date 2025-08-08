7bea55a492e95c07b120f3d86d1fa26cc303a911
webcatzz <71499717+webcatzz@users.noreply.github.com>
2025-08-08 11:31:42 +0400

    fixed dash bar appearing in rest phase

--- scenes/route/phases/rest/rest.gd
+++ scenes/route/phases/rest/rest.gd
@@ -4,4 +4,4 @@ extends Phase
 func _ready() -> void:
 	var repair: int = randi_range(2, 4)
 	Game.health += repair
-	$VBoxContainer/RepairAmount.text = "+%d hp" % repair
+	$Background/VBox/RepairAmount.text = "+%d hp" % repair
